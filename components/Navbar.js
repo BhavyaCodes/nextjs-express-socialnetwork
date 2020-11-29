66107a8c338ea07efa70fd62da79a2f4e320aa8e
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-29 22:34:36 +0530

    add navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+      <div>
+        <a href="/auth/google">Login</a>
+        <a href="/api/logout">Logout</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
