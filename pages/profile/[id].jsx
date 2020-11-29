9bd12d001e3eebe5ff9b237327ee86ce2d75a73c
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-30 04:29:57 +0530

    fetch profile from profile/[id] page

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,15 +1,32 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 
 const ProfilePage = () => {
   const { id } = useRouter().query;
-  console.log(id);
 
   const [profile, setProfile] = useState({ user: null, loading: true });
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    const getProfile = async () => {
+      try {
+        const res = await axios.get(`/api/profile/${id}`);
+        setProfile({ user: res.data, loading: false });
+      } catch (e) {
+        console.log(e.response.status);
+      }
+    };
+    getProfile();
+  }, [id]);
+
+  console.log(profile);
 
   return <div>Profile Page</div>;
 };
 
 export default ProfilePage;
+
+// console.log(e.response.status);
