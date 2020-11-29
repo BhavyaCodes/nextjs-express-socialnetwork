46c4fa83aee9ae0bfb87b1c81fe49e0a1176d3ea
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-29 19:09:48 +0530

    add express mongoose boilerplate

---
+++ server/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const next = require("next");
+const mongoose = require("mongoose");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/a", (req, res) => {
+    return app.render(req, res, "/a", req.query);
+  });
+
+  server.get("/b", (req, res) => {
+    return app.render(req, res, "/b", req.query);
+  });
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("connected to mongodb");
+      server.listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+      });
+    });
+});
