80962a063dbe1e24d0ddce180810f6e26c7faa98
Juggernau9 <xbhavyatomar@gmail.com>
2020-12-10 22:58:43 +0530

    scaffold multer middleware

--- server/middlewares/imageUpload.ts
+++ server/middlewares/imageUpload.ts
@@ -1,4 +1,33 @@
 import multer from "multer";
-import cloudinary from "cloudinary";
-import {} from "multer-storage-cloudinary";
-import uuid from "uuid";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v4 as uuidv4 } from "uuid";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "posts",
+    format: async (req, file) => "png",
+    public_id: (req, file) => uuidv4(),
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+module.exports = multer({ storage: storage, fileFilter: fileFilter });
